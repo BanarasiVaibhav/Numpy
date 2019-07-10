64f3cf2f534f02609d0b573b6c46edce1cc838f4
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-07-10 23:50:32 +0530

    Create first_code.py

---
+++ first_code.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+x=np.array([1,2,3],np.int16)
+print(x)
+
+y=np.array([[1,2,3],[4,5,6]],np.int16)
+print(y)
+
+
+print(y[:,0])
+print(y[:,1])
+print(y[:,2])
+
+#slicing columnb wise
